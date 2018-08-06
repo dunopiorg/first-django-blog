3e380c3055dfc7947c5e3dc9751b9344c030b8d0
DESKTOP-O7VTA6V\Lab2AI <jhwskorg@gmail.com>
2018-08-06 18:03:31 +0900

    no message

--- blog/views.py
+++ blog/views.py
@@ -194,7 +194,7 @@ def test(request):
     # args = record_app.test_get_team()
     player_result = record_app.get_player_event_dict(game_id, hitter_code=hitter)
     print(player_result)
-    response = JsonResponse({'status': 'OK', 'message': 'good'}, status=200)
+    response = JsonResponse(player_result, status=200)
     # render(request, 'blog/test_team_info.html', player_result)
     return response
 
