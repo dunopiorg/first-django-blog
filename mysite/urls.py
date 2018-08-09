7a3c53a110873768d8d0d1239881117b488b82e8
DESKTOP-O7VTA6V\Lab2AI <jhwskorg@gmail.com>
2018-08-09 15:55:47 +0900

    url edited

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,6 @@ from mysite import views
 urlpatterns = [
     url(r'^$', views.login_redirect, name='login_redirect'),
     url(r'^admin/', admin.site.urls),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^blog/', include('blog.urls')),
     url(r'^article/', include('blog.urls')),
 ]
