eafcf14cf694eaca26eecba1c71c1ea4e8917f61
DESKTOP-O7VTA6V\Lab2AI <jhwskorg@gmail.com>
2018-08-10 10:48:06 +0900

    stadium 에러수정

--- blog/views.py
+++ blog/views.py
@@ -246,7 +246,6 @@ def futures(request, game_id):
         if args['success']:
             article_dict_v1 = RecordApp().set_article_v1_to_db(args)
             stadium_text = RecordApp().get_stadium_text(game_id)
-            article_dict_v1['article'] += stadium_text
         else:
             return render(request, 'blog/futures_article.html', {'status': 'ERROR', 'error': args['status']})
     else:
@@ -258,7 +257,7 @@ def futures(request, game_id):
         if args_v2['success']:
             article_dict_v2 = RecordApp().set_article_v2_to_db(args_v2)
             stadium_text = RecordApp().get_stadium_text(game_id)
-            article_dict_v2['article'] += stadium_text
+            article_dict_v2['article'] = article_dict_v2['article'].rstrip('\n\n') + stadium_text
         else:
             return render(request, 'blog/futures_article.html', {'status': 'ERROR', 'error': args_v2['status']})
     else:
