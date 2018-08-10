3db13ee6e86b6f6ba5129117d4703ceebd0d0d58
DESKTOP-O7VTA6V\Lab2AI <jhwskorg@gmail.com>
2018-08-10 13:54:10 +0900

    문단 테스트 코드

--- blog/controllers.py
+++ blog/controllers.py
@@ -148,8 +148,9 @@ class RecordApp(object):
                 result_dict['title'] = info_dict['text']
             else:
                 if info_dict['info']:
-                    print(i, info_dict['info'])
                     if prev_inning == info_dict['info']['inning']:
+                        print('i=>', i, 'Dict=>', info_dict['text'])
+                        print('article list', article_list[i - 1])
                         article_list[i - 1] += info_dict['text']
                     else:
                         article_list.append(info_dict['text'])
