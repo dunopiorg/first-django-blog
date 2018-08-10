cb05cc34b5d9aa5ee8de8d15f64bca43b36c84ab
DESKTOP-O7VTA6V\Lab2AI <jhwskorg@gmail.com>
2018-08-10 13:51:11 +0900

    문단 테스트

--- blog/controllers.py
+++ blog/controllers.py
@@ -148,12 +148,12 @@ class RecordApp(object):
                 result_dict['title'] = info_dict['text']
             else:
                 if info_dict['info']:
-                    info = info_dict['info']['info']
-                    if prev_inning == info['inning']:
+                    print(i, info_dict['info'])
+                    if prev_inning == info_dict['info']['inning']:
                         article_list[i - 1] += info_dict['text']
                     else:
                         article_list.append(info_dict['text'])
-                        prev_inning = info['inning']
+                        prev_inning = info_dict['info']['inning']
                 else:
                     article_list.append(info_dict['text'])
 
